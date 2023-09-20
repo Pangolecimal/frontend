c4da7652ff11fdcdf8fb0ca39aa261fe2870b82d
Pangolecimal <domkuzaleza@gmail.com>
2023-09-20 23:31:47 +0300

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
-<script>
-    let counter = 0;
-</script>
-<style>
-    button {
-        background: green;
-        color:#FFF;
-    }
-</style>
+<h1>Home</h1>
 
-I'm counting ... {counter}
-<button on:click={() => {counter++}}>Count up</button>
+<h2>There is nothing here</h2>
+
+<p>except for some <b><i>lorem</i></b></p>
+
+<p>
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero odio at, facilis possimus temporibus sed nisi omnis,
+	minima doloremque tempore placeat dolores, laborum dolorum iusto deleniti magni fuga amet! Repudiandae repellendus hic
+	explicabo nesciunt deleniti repellat, maxime cumque veritatis quaerat fugit? Perspiciatis, at nobis. Eligendi fugit
+	pariatur dolor earum recusandae sapiente. Corrupti reprehenderit, sequi iusto ea nostrum reiciendis vitae eveniet
+	architecto alias. Aliquam cupiditate dolore laboriosam quibusdam. Minus ipsam expedita similique deserunt quae facilis
+	sequi et qui praesentium quam! Aliquam veniam eius, nesciunt sunt esse sapiente possimus ab deserunt aperiam vel sed,
+	quas pariatur cumque. Nam quia nulla quis eveniet?
+</p>
